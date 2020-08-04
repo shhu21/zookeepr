97540a0d3725bf72cce83a26dedf23f18d7b9219
shhu21 <hu.sara21@gmail.com>
2020-08-03 23:15:32 -0700

    adds zookeepers

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const animalRoutes = require('../apiRoutes/animalRoutes');
+const zookeeperRoutes = router.use(require('./zookeeperRoutes'));
 
 router.use(animalRoutes);
+router.use(zookeeperRoutes);
 
 module.exports = router;
